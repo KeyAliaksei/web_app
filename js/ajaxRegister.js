72276d1e03aad0f417bb402ff8ea5f753c720f08
aliakseikukhta <aliakseikukhta@gmail.com>
2023-03-05 00:18:33 +0200

    registration added

---
+++ js/ajaxRegister.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const form = document.getElementById('formReg');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const currentForm = e.currentTarget;
+        console.log('Form submited');
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "/web_app/registration/register");
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                console.log(xhr.status); //если статус 200
+                console.log(xhr.responseText); //
+            }};
+
+        let data = {
+                  "name": document.getElementById("name").value,
+                  "surname": document.getElementById("surname").value,
+                  "email": document.getElementById("email").value,
+                  "login": document.getElementById("login").value,
+                  "password": document.getElementById("password").value,
+                };
+
+        xhr.send(JSON.stringify(data));
+    })
+})
